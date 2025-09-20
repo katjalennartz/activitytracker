a1e40d50b9422459b12262a75d383df31598aa85
Katja <47885564+katjalennartz@users.noreply.github.com>
2025-09-20 19:12:39 +0200

    Add files via upload

--- upload/inc/tasks/at_blacklist.php
+++ upload/inc/tasks/at_blacklist.php
@@ -13,7 +13,7 @@ function task_at_blacklist($task)
     if ($mybb->settings['activitytracker_bl_turnus'] == '2weekly') {
         $now = new DateTime();
         // aktuelle Kalenderwoche
-        $weekNumber = (int) $now->format('W'); 
+        $weekNumber = (int) $now->format('W');
 
         // nur jede 2. Woche
         if ($weekNumber % 2 !== 0) {
@@ -21,7 +21,7 @@ function task_at_blacklist($task)
             return;
         }
     }
-    
+
     $icelist =  $mybb->settings['activitytracker_ice'];
     $ice = 0;
     $now = new DateTime();
@@ -57,23 +57,22 @@ function task_at_blacklist($task)
         $userinfo = get_user($uid);
         $username = $userinfo['username'];
 
+        $fetchlastdate = NULL;
+
         if ($userinfo['away'] != 0) {
-            //wir holen uns das letzte datum, wo der user abwesend gemeldet war
-            $fetchlastdate = $db->fetch_field($db->simple_select("at_blacklist", "away_last", "uid = '{$uid}'", array("order by" => "max(away_last)", "limit" => 1)), "");
-            //Der user ist dieses Mal abwesend gemeldet
             $away_this = $time;
-            //im last_away steht nichts drin - er war vorher noch nie abwesend gemeldet
 
-            //es gibt ein last_away
-
-            //Der User war noch nie abwesend gemeldet während der Blacklist
-            // if (is_null($fetchlastdate)) {
-            //     $fetchlastdate = $time;
-            // } else {
-            // //Der User war schon einmal abgemeldet
-
-            // }
+            // Prüfen, ob es schon Blacklist-Einträge gibt
+            $check_previous = $db->simple_select("at_blacklist", "away_this, away_last", "uid = '{$uid}'", array("order_by" => "blid DESC", "limit" => 1));
+            if ($previous_entry = $db->fetch_array($check_previous)) {
+                //Wenn das letzte mal während der Blacklist away gewesen, dann speichern wir das datum im away_last
+                //wenn nicht, dann übernehmen wir den alten wert (ist null, wenn noch nie away)
+                $fetchlastdate = !empty($previous_entry['away_this']) ? $previous_entry['away_this'] : $previous_entry['away_last'];
+            }
+        } else {
+            $away_this = NULL;
         }
+
         // if (is_member($usergroups)) {
         //checken ob der User auf der BL stehen würde
         $blarray = activitytracker_check_blacklist($uid);
@@ -129,9 +128,11 @@ function task_at_blacklist($task)
                 );
             }
         }
-        var_dump($blarray);
+
         if (!empty($bl_entry)) {
             $db->insert_query("at_blacklist", $bl_entry);
         }
     }
+    //datum eintragen, für die letzte ausführung 
+    $db->insert_query("at_bl_lastrun", array("bl_lastrun" => $time));
 }
---
+++ upload/inc/tasks/at_blacklist_status.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Blacklist Status - Deaktiviert die Blacklist nach Ablauf der Deadline
+ */
+
+function task_at_blacklist_status($task)
+{
+    global $db, $mybb, $lang;
+
+    // Task nur wenn die Blacklist aktiv ist
+    if ($mybb->settings['activitytracker_bl_activ'] != 1) {
+        return;
+    }
+
+    // Deadline aus den Settings holen (Tage)
+    $deadlineDays = (int)$mybb->settings['activitytracker_bl_deadline'];
+    if ($deadlineDays <= 0) {
+        return; //abfangen wenn ungültiger wert oder leer
+    }
+
+    // Datum aus der lastrun tabelle holen
+    $get_date_query = $db->simple_select("at_bl_lastrun", "bl_lastrun", "", array("order_by" => "blid DESC", "limit" => 1));
+    $get_date = $db->fetch_field($get_date_query, "bl_lastrun");
+
+    if (empty($get_date)) {
+        return; // kein datum gefunden
+    }
+
+    // datetime zu timestamp zum rechnen
+    $lastRun = strtotime($get_date);
+    $now = TIME_NOW;
+
+    // Ablaufzeit berechnen
+    $expiryTime = $lastRun + ($deadlineDays * 86400);
+
+    // ist die frist abgelaufen? 
+    if ($now >= $expiryTime) {
+        //setting speichern
+        $db->update_query("settings", ["value" => 0], "name='activitytracker_bl_activ'");
+        rebuild_settings();
+
+        add_task_log($task, "Aktivitätsplugin: Blacklist automatisch deaktiviert.");
+    } else {
+        add_task_log($task, "Aktivitätsplugin: Blacklist bleibt aktiv (noch nicht abgelaufen).");
+    }
+}
