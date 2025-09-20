9973920c2eeada48f50c295794e7cd606cecbf66
Katja <47885564+katjalennartz@users.noreply.github.com>
2025-09-20 19:10:47 +0200

    Create activitytracker.lang.php

---
+++ upload/inc/languages/deutsch_du/activitytracker.lang.php
@@ -0,0 +1,25 @@
+<?php
+$l['ucp_reason_noingamestart'] = 'Kein Ingameeinstieg';
+$l['ucp_reason_tooldscenes'] = 'Der letzte Post ist zu lange her';
+$l['ucp_reason_noapplication'] = 'Kein Steckbrief eingereicht';
+$l['ucp_reason_noingamescene'] = 'Keine offenen Ingameszenen';
+$l['ucp_reason_default'] = 'Unbekannter Grund';
+
+$l['blacklist_stroked'] = '(gestrichen)';
+$l['blacklist_application_in'] = '(Bewerbung eingereicht)';
+$l['blacklist_posted'] = '(hat gepostet: {1})';
+
+$l['blacklist_status_active'] = '<a href="misc.php?action=blacklist_at">aktiv</a>';
+$l['blacklist_status_inactive'] = 'nicht aktiv';
+
+$l['activitytracker_bl_index_reminder_info'] = 'Die aktuelle <a href="misc.php?action=blacklist_at">Blacklist</a> ist draußen.';
+$l['activitytracker_bl_index_reminder_warning'] = 'steht auf der Blacklist.';
+$l['activitytracker_bl_index_reminder_warning_multi'] = 'stehen auf der Blacklist.';
+
+$l['blacklist_turnus_weekly'] = "wöchentlich";
+$l['blacklist_turnus_2weekly'] = "alle 2 Wochen";
+$l['blacklist_turnus_monthly'] = "monatlich";
+$l['blacklist_turnus_monthly_lastmonth'] = "monatlich(im Juli für Juni... etc)";
+$l['blacklist_turnus_particular']  = "in gewählten Monaten: {1}. Immer am {2}.";
+$l['blacklist_turnus_manuel']  = "nach manueller Ausführung des Teams.";
+$l['blacklist_turnus_unknown']  = "unbekannt";
